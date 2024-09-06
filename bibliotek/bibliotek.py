7c98d5b5bbc58471b0a98e0bb21408e93d54cd3a
Hedda-Amalie <142905687+Hedda-Amalie@users.noreply.github.com>
2024-09-06 12:29:03 +0200

    start

---
+++ bibliotek/bibliotek.py
@@ -0,0 +1,10 @@
+import json
+
+with open("bibliotek/brukere.json") as f:
+    file = json.load(f)
+    print(file)
+    
+print(brukere["fornavn"])
+
+with open("bibliotek/brukere.json", "w") as f:
+    json.dump()
